012447c5c88c95faa8a7792f545a0e861994bcf3
payamaya <paulyashou81@gmail.com>
2022-09-20 20:49:24 +0200

    my-app-figma2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ function App() {
   return (
     <div className='App'>
       <h1>My-App-Figma2</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae nobis aperiam ducimus illum, unde assumenda ipsam. Veniam cumque molestias deleniti?</p>
     </div>
   )
 }
