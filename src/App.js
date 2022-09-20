d1a6fba8cea4f67c5e97d5c922dc97437a330136
payamaya <paulyashou81@gmail.com>
2022-09-20 20:54:52 +0200

    commit my-app-figma2

--- src/App.js
+++ src/App.js
@@ -2,8 +2,17 @@ function App() {
   return (
     <div className='App'>
       <h1>My-App-Figma2</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi esse, cupiditate error ea at molestias voluptatum mollitia iste porro natus.</p>
+      <div>
+        <section>
+          <nav></nav>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore
+            eius assumenda amet odit, reiciendis at praesentium ut quis omnis
+            culpa eveniet, alias, similique exercitationem corrupti.
+          </p>
+        </section>
       </div>
+    </div>
   )
 }
 
